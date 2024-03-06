69ba7d033cb1959d11612b3aa5906f5f74738214
agbobli5373 <agbobliisaackwadzo@gmail.com>
2024-03-06 15:10:28 +0000

    Update Python base image version to 3.10

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
-cd /app
-source .venv/bin/activate
+# #!/bin/sh
+# cd /app
+# source .venv/bin/activate
 
 
-if [[ "$1" == "celery-worker" ]]; then
-    celery -A todomir worker  -l INFO
-elif [[ "$1" == "celery-beat" ]]; then
-    celery -A todomir worker --beat -l INFO
-else
-    python manage.py collectstatic --noinput
-    python manage.py migrate 
-    python manage.py nginx
-    gunicorn --config $GUNICORN_CONFIG
-fi
+# if [[ "$1" == "celery-worker" ]]; then
+#     celery -A todomir worker  -l INFO
+# elif [[ "$1" == "celery-beat" ]]; then
+#     celery -A todomir worker --beat -l INFO
+# else
+#     python manage.py collectstatic --noinput
+#     python manage.py migrate 
+#     python manage.py nginx
+#     gunicorn --config $GUNICORN_CONFIG
+# fi
